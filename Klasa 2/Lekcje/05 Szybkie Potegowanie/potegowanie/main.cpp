304ae5fb72ee908dd661370a286de2154073ab23
“Milyzak987” <milosz.zakrzewski02@gmail.com>
2023-10-17 20:47:41 +0200

    easy, fast power

---
+++ Klasa 2/Lekcje/05 Szybkie Potegowanie/potegowanie/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+const int MOD = 10000007;
+
+long long power(long long a, long long n) {
+    long long res = 1;
+    while (n > 0) {
+        if (n % 2 == 1) {
+            res = (res * a) % MOD;
+        }
+        a = (a * a) % MOD;
+        n /= 2;
+    }
+    return res;
+}
+
+int main() {
+    long long n, a, b;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        cout << power(a, b) << "\n";
+    }
+}
