e331cc2704e9d7ffafa206ab2a902b29361f7e07
“Milyzak987” <milosz.zakrzewski02@gmail.com>
2023-10-24 08:48:15 +0200

    promocja 50

--- Klasa-2/Lekcje/04_Binsearch/promocja/main.cpp
+++ Klasa-2/Lekcje/04_Binsearch/promocja/main.cpp
@@ -5,42 +5,42 @@ using namespace std;
 
 const int MAXN = 300007;
 
-vector<pair<int, int>> people[MAXN];
-int cities[MAXN];
+vector<pair<int, int>> ppl[MAXN];
+int cit[MAXN];
 
 int main() {
     int n, m, z;
     inicjuj(&n, &m, &z);
 
     for (int i = 1; i <= n; i++) {
-        cities[i] = i;
-        people[i].push_back(make_pair(0, i));
+        cit[i] = i;
+        ppl[i].push_back(make_pair(0, i));
     }
 
     for (int i = 1; i <= m; i++) {
         int a, b;
         pobierz_promocje(i, &a, &b);
 
-        int pa = cities[a];
-        int pb = cities[b];
+        int pa = cit[a];
+        int pb = cit[b];
 
-        int tmp = cities[a];
-        cities[a] = cities[b];
-        cities[b] = tmp;
+        int tmp = cit[a];
+        cit[a] = cit[b];
+        cit[b] = tmp;
 
-        people[pa].push_back(make_pair(i, b));
-        people[pb].push_back(make_pair(i, a));
+        ppl[pa].push_back(make_pair(i, b));
+        ppl[pb].push_back(make_pair(i, a));
     }
 
     for (int i = 0; i < z; i++) {
         int a, b;
         pobierz_zapytanie(&a, &b);
         int res = -1;
-        for (const auto& promo : people[a]) {
-            if (promo.first > b) {
+        for (auto prom : ppl[a]) {
+            if (prom.first > b) {
                 break;
             }
-            res = promo.second;
+            res = prom.second;
         }
         odpowiedz(res);
     }
