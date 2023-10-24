bd3a82b5adb573ed2489478e1ab833152751faef
“Milyzak987” <milosz.zakrzewski02@gmail.com>
2023-10-24 08:55:50 +0200

    binsearch, 100

--- Klasa-2/Lekcje/04_Binsearch/promocja/main.cpp
+++ Klasa-2/Lekcje/04_Binsearch/promocja/main.cpp
@@ -1,6 +1,7 @@
 #include <vector>
-
+#include <algorithm>
 #include "cprolib.h"
+
 using namespace std;
 
 const int MAXN = 300007;
@@ -32,16 +33,26 @@ int main() {
         ppl[pb].push_back(make_pair(i, a));
     }
 
+    for (int i = 1; i <= n; i++) {
+        sort(ppl[i].begin(), ppl[i].end());
+    }
+
     for (int i = 0; i < z; i++) {
         int a, b;
         pobierz_zapytanie(&a, &b);
         int res = -1;
-        for (auto prom : ppl[a]) {
-            if (prom.first > b) {
-                break;
+
+        int left = 0, right = ppl[a].size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (ppl[a][mid].first <= b) {
+                res = ppl[a][mid].second;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-            res = prom.second;
         }
+
         odpowiedz(res);
     }
 }
