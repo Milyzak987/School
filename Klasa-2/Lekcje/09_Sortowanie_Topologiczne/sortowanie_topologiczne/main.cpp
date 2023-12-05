d7c5b4091102eac2930b3a5bf964e8d8204ae30d
“Milyzak987” <milosz.zakrzewski02@gmail.com>
2023-12-05 08:59:40 +0100

    topology sort

---
+++ Klasa-2/Lekcje/09_Sortowanie_Topologiczne/sortowanie_topologiczne/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+vector<int> graph[1000007];
+vector<int> degree(1000007, 0);
+
+void toposort(int n) {
+    queue<int> ready;
+    vector<int> order;
+    for (int i = 0; i < n; i++) {
+        if (degree[i] == 0) ready.push(i);
+    }
+
+    if (ready.size() < 1) {
+        cout << "NIE";
+        return;
+    }
+
+    while (!ready.empty()) {
+        int v = ready.front();
+        ready.pop();
+        order.push_back(v);
+        for (auto u : graph[v]) {
+            degree[u]--;
+            if (degree[u] == 0) ready.push(u);
+        }
+    }
+    if (order.size() != n)
+        cout << "NIE";
+    else {
+        cout << "TAK\n";
+        for (auto x : order) cout << x + 1 << " ";
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        graph[a - 1].push_back(b - 1);
+        degree[b - 1]++;
+    }
+    toposort(n);
+}
