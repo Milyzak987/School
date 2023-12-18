965e0b61c4bb4c0857cb2f06f2f93ca2a1fa31a4
“Milyzak987” <milosz.zakrzewski02@gmail.com>
2023-12-18 22:54:04 +0100

    dfs, postored, ssfs

--- smallPREOI/Day1/wydra/main.cpp
+++ smallPREOI/Day1/wydra/main.cpp
@@ -1,43 +1,93 @@
-//60 punktow
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-const int MAXN = 100002;
-
+const int MAXN = 1000005;
+int sscNumber[MAXN];
+int sscSize[MAXN];
+int dp[MAXN];
+vector<bool> visited(MAXN);
+vector<int> postOrder;
 vector<int> graph[MAXN];
-vector<bool> visited(MAXN, false);
-int edges = 0;
+vector<int> reversedGraph[MAXN];
+vector<int> newGraph[MAXN];
 
-
-int dfs(int v) {
+void ssfs(int v, int x) {
     visited[v] = true;
-    for (int u : graph[v]) {
-        edges++;
-        if (!visited[u]) {
-            dfs(u);
+    sscNumber[v] = x;
+    for (int i = 0; i < reversedGraph[v].size(); ++i) {
+        if (!visited[reversedGraph[v][i]]) {
+            ssfs(reversedGraph[v][i], x);
         }
     }
-    return edges;
+}
+
+void dfs(int v) {
+    visited[v] = true;
+    for (int i = 0; i < graph[v].size(); ++i) {
+        if (!visited[graph[v][i]]) {
+            dfs(graph[v][i]);
+        }
+    }
+    postOrder.push_back(v);
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
     int n, m;
     cin >> n >> m;
 
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; ++i) {
         int a, b;
         cin >> a >> b;
-        graph[a].push_back(b);
+        graph[b].push_back(a);
+        reversedGraph[a].push_back(b);
     }
 
-    for (int i = 1; i <= n; i++) {
-        cout << dfs(i) << "\n";
-        fill(visited.begin(), visited.end(), false);
-        edges = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (!visited[i]) {
+            dfs(i);
+        }
+    }
+
+    for (int i = 0; i <= n; ++i) {
+        visited[i] = false;
+    }
+
+    int counter = 0;
+    while (!postOrder.empty()) {
+        int backValue = postOrder.back();
+        if (!visited[backValue]) {
+            ssfs(backValue, counter);
+            postOrder.pop_back();
+            counter++;
+        } else {
+            postOrder.pop_back();
+        }
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j < reversedGraph[i].size(); j++) {
+            if (sscNumber[i] != sscNumber[reversedGraph[i][j]]) {
+                newGraph[sscNumber[i]].push_back(sscNumber[reversedGraph[i][j]]);
+            } else {
+                sscSize[sscNumber[i]]++;
+            }
+        }
+    }
+
+    for (int i = 0; i < counter; ++i) {
+        int maxVal = 0;
+        for (int j = 0; j < newGraph[i].size(); j++) {
+            maxVal = max(dp[newGraph[i][j]] + 1, maxVal);
+        }
+        dp[i] = maxVal + sscSize[i];
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        cout << dp[sscNumber[i]] << "\n";
     }
 
     return 0;
