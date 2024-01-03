a254a3b56d77b0850f43d8bb0d9d95d819a7a63c
“Milyzak987” <milosz.zakrzewski02@gmail.com>
2024-01-03 22:19:59 +0100

    binsearch

--- smallPREOI/Day3/pizza/main.cpp
+++ smallPREOI/Day3/pizza/main.cpp
@@ -3,16 +3,33 @@
 #include <vector>
 using namespace std;
 
-const int MAXN = 1007;
+constexpr int MAXN = 200007;
+vector<long long> lengthA(MAXN), lengthB(MAXN);
+long long n, k;
 
-vector<long long> lengthA(MAXN);
-vector<long long> lengthB(MAXN);
-vector<long long> areas(MAXN* MAXN);
+long long solve(long long x) {
+    long long res = 0, a = 0;
+    for (int b = n - 1; b >= 0; b--) {
+        while (a < n && lengthA[a] * lengthB[b] < x) a++;
+        res += n - a;
+    }
+    return res;
+}
+
+void binsearch() {
+    long long l = lengthA[0] * lengthB[0], r = lengthA[n - 1] * lengthB[n - 1];
+    while (l <= r) {
+        long long mid = (l + r) / 2;
+        if (solve(mid) >= k) l = mid + 1;
+        else r = mid - 1;
+    }
+    cout << r;
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    long long a, b, n, k;
+    long long a, b;
     cin >> a >> b >> n >> k;
 
     long long curr = 0, x;
@@ -31,13 +48,9 @@ int main() {
     }
     lengthB[n] = b - x;
 
-    long long y = 0;
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= n; j++) {
-            areas[y] = lengthA[i] * lengthB[j];
-            y++;
-        }
-    }
-    sort(areas.begin(), areas.begin() + (n + 1) * (n + 1), greater());
-    cout << areas[k - 1];
+    n++;
+    sort(lengthA.begin(), lengthA.begin() + n);
+    sort(lengthB.begin(), lengthB.begin() + n);
+
+    binsearch();
 }
