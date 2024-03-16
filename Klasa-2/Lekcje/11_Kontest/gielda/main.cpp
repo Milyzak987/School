d487112c41a1834808633f6707895f1733a1642a
Milyzak987 <milosz.zakrzewski02@gmail.com>
2024-03-16 12:16:51 +0100

    dp

--- Klasa-2/Lekcje/11_Kontest/gielda/main.cpp
+++ Klasa-2/Lekcje/11_Kontest/gielda/main.cpp
@@ -1,28 +1,47 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
 
-const int MAXN = 1e5 + 7;
+const int MAXN = 1e4 + 7;
+const int MAXK = 2e3 + 7;
 vector<int> prices(MAXN);
-vector<int> max_money(MAXN);
+vector<vector<long long>> dp(MAXK, vector<long long>(MAXN));
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+
     int n, k;
     cin >> n >> k;
     for (int i = 0; i < n; i++) {
         cin >> prices[i];
     }
-    int maxx = 0;
-    for (int i = 0; i < n; i++) {
-        int money = 0;
-        for (int j = i; j < n; j++) {
-            int diff = prices[j] - prices[i];
-            money = max(money, diff);
+
+    int z = 2 * k;
+    for (int i = 1; i <= z; i++) {
+        if (i % 2 == 0) {
+            dp[i][0] = dp[i - 1][0] + prices[0];
+        } else {
+            dp[i][0] = dp[i - 1][0] - prices[0];
         }
-        maxx = max(maxx, money);
+        for (int j = 2; j <= n; j++) {
+            if (i % 2 == 1) {
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j] - prices[j - 1]);
+            } else {
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j] + prices[j - 1]);
+            }
+        }
     }
-    cout << maxx;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= z; j++) {
+            if (j % 2 == 1) {
+                dp[j][i] = max(dp[j][i - 1], dp[j - 1][i] - prices[i - 1]);
+            } else {
+                dp[j][i] = max(dp[j][i - 1], dp[j - 1][i] + prices[i - 1]);
+            }
+        }
+    }
+
+    cout << dp[z][n];
 }
