e3ef6c6cc41918bbd0d5ee95c39ee2ce896ecb13
“Milyzak987” <milosz.zakrzewski02@gmail.com>
2024-09-20 08:30:04 +0200

    New year

---
+++ Klasa-3_(24-25)/Lekcje/wyprawa/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAXN = 200000;
+int num[MAXN];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> num[i];
+    }
+}
