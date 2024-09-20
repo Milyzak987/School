c4f011c3035e503178adac720c63ed7985986581
“Milyzak987” <milosz.zakrzewski02@gmail.com>
2024-09-20 08:59:32 +0200

    wyprawa

--- Klasa-3_(24-25)/Lekcje/wyprawa/main.cpp
+++ Klasa-3_(24-25)/Lekcje/wyprawa/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-const int MAXN = 200000;
+const int MAXN = 200007;
 int num[MAXN];
+int desc[MAXN];
+int asc[MAXN];
+int mer[MAXN];
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -13,4 +17,26 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> num[i];
     }
+    asc[0] = 1;
+    for (int i = 1; i < n; i++) {
+        if(num[i] > num[i-1]){
+            asc[i] = asc[i-1] + 1;
+        } else{
+            asc[i] = 1;
+        }
+    }
+    desc[n-1] = 1;
+    for (int i = n-2; i >= 0; i--) {
+        if(num[i] > num[i+1]){
+            desc[i] = desc[i+1] + 1;
+        } else{
+            desc[i] = 1;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        mer[i] = desc[i] + asc[i];
+    }
+    int res;
+    res = *max_element (mer, mer + n);
+    cout << res - 1;
 }
