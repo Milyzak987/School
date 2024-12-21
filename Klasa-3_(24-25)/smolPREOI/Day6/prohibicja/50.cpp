9b300276962539484e85e685e29133b555ba400b
“Milyzak987” <milosz.zakrzewski02@gmail.com>
2024-12-21 20:16:29 +0100

    sth

--- Klasa-3_(24-25)/smolPREOI/Day6/prohibicja/50.cpp
+++ Klasa-3_(24-25)/smolPREOI/Day6/prohibicja/50.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <unordered_map>
-#include <vector>
 using namespace std;
 
-// const int MAXN = 5e5 + 7;
+const int MAXN = 5e5 + 7;
 const int INF = 1e9 + 7;
-// int hungary[MAXN];
+int hungary[MAXN];
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -14,30 +12,24 @@ int main() {
     int n, t;
     cin >> n >> t;
 
-    int maxx = -INF, minx = INF;
+    int minn = INF;
     for (int i = 0; i < n; i++) {
         int a;
         cin >> a;
-        if ((a % t) > (t / 2)) {
-            // hungary[i] = (a % t) - t;
-            a = (a % t) - t;
-        } else {
-            // hungary[i] = a % t;
-            a = a % t;
+        hungary[i] = a % t;
+        minn = min(minn, hungary[i]);
+    }
+
+    int maxx = -INF, minx = INF;
+    for (int i = 0; i < n; i++) {
+        if (hungary[i] - minn > minn - (hungary[i] - t)) {
+            hungary[i] = hungary[i] - t;
         }
-        // maxx = max(maxx, hungary[i]);
-        maxx = max(maxx, a);
-        // minx = min(minx, hungary[i]);
-        minx = min(minx, a);
+        maxx = max(maxx, hungary[i]);
+        minx = min(minx, hungary[i]);
     }
 
     int dif = maxx - minx;
-
-    // for (int i = 0; i < n; i++) {
-    //     cout << hungary[i] << " ";
-    // }
-    // cout << "\n";
-
     cout << dif / 2 + (dif % 2 != 0);
 
     return 0;
