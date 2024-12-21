11033f9c832df5167acdd8074cfdfefb63cf4ae9
“Milyzak987” <milosz.zakrzewski02@gmail.com>
2024-12-21 20:50:58 +0100

    wieze 100

--- Klasa-3_(24-25)/smolPREOI/Day1/wieze/main.cpp
+++ Klasa-3_(24-25)/smolPREOI/Day1/wieze/main.cpp
@@ -1,57 +1,66 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <climits>
 using namespace std;
 typedef long long ll;
-typedef pair<ll, ll> pll;
 
 const ll MAXN = 1e6 + 7;
-vector<pll> cost(MAXN);
+vector<int> cost1(MAXN);
+vector<int> cost2(MAXN);
+vector<int> schem(MAXN, INT_MAX);
+vector<int> dp(MAXN);
 
-bool comp(const pll a, const pll b) {
-    if (a.first - a.second == b.first - b.second)
-        return a.first < b.first;
-    return a.first - a.second < b.first - b.second;
+void schematics(int n) {
+    for (int i = 1; i <= n; i++) {
+        schem[cost1[i]] = min(schem[cost1[i]], cost1[i] - cost2[i]);
+    }
+    for (int i = 1; i < MAXN; i++) {
+        schem[i] = min(schem[i], schem[i - 1]);
+    }
 }
 
-ll binarySearch(ll left, ll right, ll b, ll i) {
-    while (left < right) {
-        ll mid = (left + right) / 2;
-        if (b - (cost[i].first - cost[i].second) * mid >= cost[i].first) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+void computeDP() {
+    for (int i = 1; i < MAXN; i++) {
+        if (schem[i] != INT_MAX) {
+            dp[i] = dp[max(0, i - schem[i])] + 2;
         }
     }
+}
 
-    return left;
+ll solve(int m) {
+    ll result = 0;
+    for (int i = 0; i < m; i++) {
+        int mass;
+        cin >> mass;
+
+        while (mass > MAXN - 1) {
+            int reduction = (mass - (MAXN - 1) - 1) / schem[MAXN - 1] + 1;
+            mass -= reduction * schem[MAXN - 1];
+            result += 2 * reduction;
+        }
+        result += dp[mass];
+    }
+    return result;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    ll n, m;
+    int n, m;
     cin >> n >> m;
-    for (ll i = 0; i < n; i++) {
-        cin >> cost[i].first;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> cost1[i];
     }
-    for (ll i = 0; i < n; i++) {
-        cin >> cost[i].second;
-    }
-    sort(cost.begin(), cost.begin() + n, comp);
-    ll exp = 0;
-    for (ll j = 0; j < m; j++) {
-        int blocks;
-        cin >> blocks;
-        for (ll i = 0; i < n; i++) {
-            if (blocks >= cost[i].first) {
-                ll p = binarySearch(1, 1e9, blocks, i);
-                blocks -= p * (cost[i].first - cost[i].second);
-                exp += 2 * p;
-            }
-        }
+    for (int i = 1; i <= n; i++) {
+        cin >> cost2[i];
     }
 
-    cout << exp;
+    schematics(n);
+    computeDP();
+
+    cout << solve(m);
+    return 0;
 }
