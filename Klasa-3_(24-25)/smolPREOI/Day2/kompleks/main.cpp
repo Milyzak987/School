91c501b991ec7e0eb80814b2e5429e9f3a6d0c1f
“Milyzak987” <milosz.zakrzewski02@gmail.com>
2024-12-21 20:53:05 +0100

    kompleks mniejszosci 100

--- Klasa-3_(24-25)/smolPREOI/Day2/kompleks/main.cpp
+++ Klasa-3_(24-25)/smolPREOI/Day2/kompleks/main.cpp
@@ -1,29 +1,52 @@
 #include <iostream>
 #include <vector>
-#include <deque>
 using namespace std;
 typedef long long ll;
+typedef pair<ll, ll> pll;
 
 const int MAXN = 1e6 + 7;
+const int R = 1 << 20;
+vector<int> tree(2 * R);
 vector<int> graph[MAXN];
-bool visited[MAXN];
-deque<int> up;
-int ans[MAXN];
+pll order[MAXN];
 
-void dfs(int v) {
-    visited[v] = 1;
-    up.push_back(v);
-    for (int u : graph[v]) {
-        if (!visited[u]) {
-            for(auto x : up){
-                if(x > u){
-                    ans[x]++;
-                }
-            }
-            dfs(u);
+void dfs(int v, int p, int &ord) {
+    ord++;
+    order[v].first = ord;
+    for (auto u : graph[v]) {
+        if (u != p) {
+            dfs(u, v, ord);
         }
     }
-    up.pop_back();
+    order[v].second = ord;
+}
+
+void update(int v) {
+    v += R;
+    tree[v] = 1;
+    while (v > 1) {
+        v /= 2;
+        tree[v] = tree[2 * v] + tree[2 * v + 1];
+    }
+}
+
+int query(int l, int r) {
+    l += R;
+    r += R;
+    int wynik = tree[l];
+    if (l != r)
+        wynik += tree[r];
+    while (l / 2 != r / 2) {
+        if (l % 2 == 0) {
+            wynik += tree[l + 1];
+        }
+        if (r % 2 == 1) {
+            wynik += tree[r - 1];
+        }
+        l /= 2;
+        r /= 2;
+    }
+    return wynik;
 }
 
 int main() {
@@ -33,15 +56,18 @@ int main() {
     int n;
     cin >> n;
 
-    for(int i = 1; i < n; i++){
+    for (int i = 0; i < n - 1; i++) {
         int a, b;
         cin >> a >> b;
         graph[a].push_back(b);
         graph[b].push_back(a);
     }
-    dfs(1);
 
-    for(int i = 1; i <= n; i++){
-        cout << ans[i] << "\n";
+    int ord = 0;
+    dfs(1, 1, ord);
+
+    for (int i = 1; i <= n; i++) {
+        cout << query(order[i].first, order[i].second) << "\n";
+        update(order[i].first);
     }
 }
