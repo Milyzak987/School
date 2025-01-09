8a6373e2c438aa86337862d8b7197eb5bef0ff5d
Milyzak987 <milosz.zakrzewski02@gmail.com>
2025-01-09 22:52:11 +0100

    krasnale 100

--- Klasa-3_(24-25)/smolPREOI/Day4/krasnale/main.cpp
+++ Klasa-3_(24-25)/smolPREOI/Day4/krasnale/main.cpp
@@ -1,7 +1,13 @@
-#include "kralib.h"
 #include <bits/stdc++.h>
+
+#include "kralib.h"
 using namespace std;
 typedef long long ll;
+
+const int MAXN = 1e5 + 7;
+int diffPairs[MAXN];
+int diffThrees[MAXN];
+int relations[MAXN];
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -10,63 +16,50 @@ int main() {
     int n = init();
     vector<int> ans(n);
 
-    int dif = n - 1, mini = 0;
-    while (dif == n - 1) {
-        mini++;
-        dif = RoznicaCzapek(mini, n - 1);
+    for (int i = 0; i <= n - 2; i++) {
+        diffPairs[i] = RoznicaCzapek(i, i + 1);
     }
-    mini--;
-    ans[mini] = 0;
-
-    int dif1, dif2;
-    // if (maxi < n - 1) {
-    //     ans[maxi + 1] = n - 1 - RoznicaCzapek(maxi, maxi + 1);
-    // }
-    // for (int i = maxi + 2; i < n; i++) {
-    //     // cout << "PYTANIE: " << maxi << " " << i << "\n";
-    //     dif1 = RoznicaCzapek(i - 1, i);
-    //     dif2 = RoznicaCzapek(i - 2, i);
-    //     // cout << ans[i - 1] + dif1 << " ? " << ans[i - 2] - dif2 << "\n";
-    //     if (ans[i - 1] + dif1 == ans[i - 2] + dif2 || dif2 == ans[i - 2] - ans[i - 1]) {
-    //         ans[i] = ans[i - 1] + dif1;
-    //     } else {
-    //         ans[i] = ans[i - 1] - dif1;
-    //     }
-    // }
-
-    // cout << "\n"<< mini << "\n";
-
-    if (mini > 0) {
-        ans[mini - 1] = RoznicaCzapek(mini - 1, mini);
+    for (int i = 0; i <= n - 3; i++) {
+        diffThrees[i] = RoznicaCzapek(i, i + 2);
     }
-    for (int i = mini - 2; i >= 0; i--) {
-        // cout << "PYTANIE: " << maxi << " " << i << "\n";
-        dif1 = RoznicaCzapek(i, i + 1);
-        dif2 = RoznicaCzapek(i, i + 2);
-        // cout << ans[i - 1] + dif1 << " ? " << ans[i - 2] - dif2 << "\n";
-        if (ans[i + 1] + dif1 == ans[i + 2] + dif2 || dif2 == ans[i + 2] - ans[i + 1]) {
-            ans[i] = ans[i + 1] + dif1;
+
+    relations[1] = -1;
+    for (int i = 2; i <= n - 1; i++) {
+        if (diffPairs[i - 2] + diffPairs[i - 1] == diffThrees[i - 2]) {
+            relations[i] = relations[i - 1];
         } else {
-            ans[i] = ans[i + 1] - dif1;
+            relations[i] = -relations[i - 1];
         }
     }
 
-    ans[mini + 1] = RoznicaCzapek(mini, mini + 1);
-    for (int i = mini + 2; i < n; i++) {
-        // cout << "PYTANIE: " << maxi << " " << i << "\n";
-        dif1 = RoznicaCzapek(i - 1, i);
-        dif2 = RoznicaCzapek(i - 2, i);
-        // cout << ans[i - 1] + dif1 << " ? " << ans[i - 2] - dif2 << "\n";
-        if (ans[i - 1] + dif1 == ans[i - 2] + dif2 || dif2 == ans[i - 2] - ans[i - 1]) {
-            ans[i] = ans[i - 1] + dif1;
-        } else {
-            ans[i] = ans[i - 1] - dif1;
+    int minx = 0, maxx = 0, mini = 0, maxi = 0;
+    ans[0] = 0;
+    for (int i = 1; i < n; i++) {
+        ans[i] = ans[i - 1] + relations[i] * diffPairs[i - 1];
+        if (ans[i] < minx) {
+            minx = ans[i];
+            mini = i;
+        }
+        if (ans[i] > maxx) {
+            maxx = ans[i];
+            maxi = i;
         }
     }
 
-    // for (auto x : ans) {
-    //     cout << x << " ";
-    // }
+    int znak = 1;
+    if (mini > maxi) {
+        swap(mini, maxi);
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] + -1 * relations[i] * diffPairs[i - 1];
+        }
+    }
+
+    int offset = 0 - ans[mini];
+
+    for (int i = 0; i < n; i++) {
+        ans[i] += offset;
+    }
+
     Odpowiedz(ans);
 
     return 0;
