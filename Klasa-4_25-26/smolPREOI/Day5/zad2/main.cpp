5d401943fa7bee80b4aac9023cfbcb24c75bd031
Milosz Zakrzewski <milosz.zakrzewski02@gmail.com>
2025-12-16 20:38:15 +0100

    Day5 templates

---
+++ Klasa-4_25-26/smolPREOI/Day5/zad2/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+
+const int MAXN;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+
+    return 0;
+}
